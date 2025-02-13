6f5aa4f2576784518fa26d85572cf435a90cc394
Igornoel <n.ishimwe4@alustudent.com>
2025-02-13 13:00:16 +0200

    Add regex data extraction implementation

---
+++ credit_cards.py
@@ -0,0 +1,7 @@
+import re
+
+def extract_credit_cards(text):
+    data = {}
+    credit_card_pattern = r'\b(?:\d{4}[-\s]?){3}\d{4}\b'
+    data['credit_cards'] = re.findall(credit_card_pattern, text)
+    return data
